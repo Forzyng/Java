656d604d26ae59731a1f42b1017519273366685a
Yaroslav <fozzynice@gmail.com>
2022-08-16 19:05:34 +0300

    Java

---
+++ welcome/run.cmd
@@ -0,0 +1,4 @@
+C:\Users\Yaroslav\.jdks\openjdk-18.0.2\bin\java.exe
+"-javaagent:D:\Programming\Setups\Spring\IntelliJ IDEA 2022.2\lib\idea_rt.jar=63820:D:\Programming\Setups\Spring\IntelliJ IDEA 2022.2\bin"
+-Dfile.encoding=UTF-8
+-classpath D:\Programming\Projects\Java\welcome\out\production\welcome Main
