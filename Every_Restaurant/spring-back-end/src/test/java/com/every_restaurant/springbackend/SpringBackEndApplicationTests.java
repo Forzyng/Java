278e15f31aa659ecc10b8bf2d1594ead7e9b185d
Forzyng <fozzynice@gmail.com>
2022-10-23 18:44:15 +0300

    Oauth2

---
+++ Every_Restaurant/spring-back-end/src/test/java/com/every_restaurant/springbackend/SpringBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.every_restaurant.springbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
